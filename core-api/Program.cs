d6dd28c3cd757047084489daca13d6a16e8a455a
gzaprzala <zaprzala.grzegorz@gmail.com>
2025-01-19 22:44:36 +0100

    init: added core-api to docker

--- core-api/Program.cs
+++ core-api/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+string aspNetCoreEnv = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
