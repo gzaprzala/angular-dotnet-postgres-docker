b3833cf6f1afce917adbdb2a83631a3d5ac19090
gzaprzala <zaprzala.grzegorz@gmail.com>
2025-03-31 14:09:51 +0200

    feat: implemented docker files for api publish

--- frontend/src/app/enums/api.enum.ts
+++ frontend/src/app/enums/api.enum.ts
@@ -1,4 +1,4 @@
 export enum API {
-  AuthApi = 'http://localhost:7055/Auth',
-  CoreApi = 'http://localhost:7054/Api',
+  AuthApi = 'https://example-domain.com/Auth',
+  CoreApi = 'https://example-domain.com/Api',
 }
