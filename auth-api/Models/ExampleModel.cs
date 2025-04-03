78aaa012afde6ee7fff8ee5251a331d8348d1350
Grzegorz Zaprzała <92335059+gzaprzala@users.noreply.github.com>
2025-04-03 14:33:51 +0200

    update: merged apis into one and enabled text compression (#12)
    * update: made app use one api
    
    * feat: enabled text compression

--- auth-api/Models/ExampleModel.cs
+++
@@ -1,9 +0,0 @@
-namespace core_api.Models;
-
-public class ExampleModel 
-{
-  public required string Id { get; set; }
-  public required string Text { get; set; }
-
-  public required int RandomNumber { get; set; }
-}
