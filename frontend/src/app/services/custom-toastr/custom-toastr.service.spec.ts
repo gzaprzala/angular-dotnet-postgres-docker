25c3d3b212c4ab36c9d3c1640d37123835987c95
gzaprzala <zaprzala.grzegorz@gmail.com>
2025-04-03 19:25:25 +0200

    refactor: updated tests for new toasts

--- frontend/src/app/services/custom-toastr/custom-toastr.service.spec.ts
+++ frontend/src/app/services/custom-toastr/custom-toastr.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { CustomToastrService } from './custom-toastr.service';
 
 describe('CustomToastrService', () => {
   let service: CustomToastrService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ToastrModule.forRoot()],
+      providers: [ToastrService],
+    });
+
     service = TestBed.inject(CustomToastrService);
   });
 
